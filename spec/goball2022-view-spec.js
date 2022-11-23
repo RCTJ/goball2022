ea80b7668339a51cc6bad4ec43958882f2c17946
SBOAAAA <fun_pu@hotmail.com>
2022-11-23 16:13:19 +0700

    edit

---
+++ spec/goball2022-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Goball2022View from '../lib/goball2022-view';
+
+describe('Goball2022View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
